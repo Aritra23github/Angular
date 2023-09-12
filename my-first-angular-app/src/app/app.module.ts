69e324d67267a6fbe0134127c5e3010935ff86d2
Aritra23github <aritragithub@gmail.com>
2023-09-13 01:04:52 +0530

    basics of components

--- my-first-angular-app/src/app/app.module.ts
+++ my-first-angular-app/src/app/app.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
   providers: [],
+  // bootstrap will tells angular which component will run first when the app will start running.
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- my-first-angular-app/src/app/server/server.component.ts
+++ my-first-angular-app/src/app/server/server.component.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
     templateUrl: './server.component.html',
 })
 
-class ServerComponent {
+export class ServerComponent {
 
 }
