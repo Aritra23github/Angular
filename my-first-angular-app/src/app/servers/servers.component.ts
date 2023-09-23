243ccab7dec8e76911820cd498211c520ac67eb7
Aritra23github <aritragithub@gmail.com>
2023-09-24 01:50:44 +0530

    event binding

--- my-first-angular-app/src/app/servers/servers.component.ts
+++ my-first-angular-app/src/app/servers/servers.component.ts
@@ -7,10 +7,20 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
   isButtonDisabled = true;
+  serverCreationStatus = "No server created!!!";
+  serverName = "";
 
   constructor() {
     setTimeout(() => {
       this.isButtonDisabled = false;
     }, 2000);
+  }
+
+  onServerCreated() {
+    this.serverCreationStatus = "Server created!!!";
+  }
+
+  generateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
